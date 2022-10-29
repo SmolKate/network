9ed2417348335f2a6dc0dac25ff3e1bfd5fc2c1d
SmolKate <Zdobnovaea@gmail.com>
2022-10-29 15:13:56 +0300

    Creation of structure of the  main page

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+          <header className = 'head'>
+            <img src='https://static.vecteezy.com/system/resources/thumbnails/008/977/302/small/simple-logo-in-the-shape-of-a-mountain-panorama-themed-logo-vector.jpg'></img>
+          </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className = 'content'>
+            
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_TNXpBwLvwobYsBplpRwvFGiL0iHONril4A&usqp=CAU'></img>
+            
+            <div>
+                ava+descripiton
+            </div>
+            <div>
+                My posts
+                <div>
+                    <div>
+                    New post
+                    </div>
+                    <div>
+                    post 1
+                    </div>
+                    <div>
+                    post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile;
